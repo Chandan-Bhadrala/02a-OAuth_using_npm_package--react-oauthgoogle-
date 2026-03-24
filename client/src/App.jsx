41dc7f1c7ba9ef3911533ba322896c6735ba2e04
Chandan Bhadrala <C.Bhadrala88@gmail.com>
2026-03-24 15:21:14 +0530

    Completed Protected Route setup.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,28 @@ import "./App.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import GoogleLogin from "./GoogleLogin";
 import Dashboard from "./Dashboard";
+import RefreshHandler from "./RefreshHandler";
 import { PageNotFound } from "./PageNotFound";
+import { useState } from "react";
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const PrivateRoute = ({ element }) => {
+    return isAuthenticated ? element : <Navigate to="/login" />;
+  };
+
   return (
     <BrowserRouter>
+      <RefreshHandler setIsAuthenticated={setIsAuthenticated} />
       <Routes>
         <Route path="/login" element={<GoogleLogin />} />
         <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/dashboard" element={<Dashboard/>} />
-        <Route path="*" element={<PageNotFound/>} />
+        <Route
+          path="/dashboard"
+          element={<PrivateRoute element={<Dashboard />} />}
+        />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
--- client/src/Dashboard.jsx
+++ client/src/Dashboard.jsx
@@ -1,6 +1,34 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const Dashboard = () => {
+  const navigate = useNavigate();
+  const [userInfo, setUserInfo] = useState(null);
+
+  useEffect(() => {
+    const data = localStorage.getItem("user-info");
+    const userData = JSON.parse(data);
+    setUserInfo(userData);
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem("user-info");
+    navigate("/");
+  };
+
   return (
-    <div>Dashboard</div>
+    <div className="flex h-screen flex-col items-center justify-center">
+      <h1>Welcome, {userInfo?.name}</h1>
+      <h3>Email: {userInfo?.email}</h3>
+      <img src={userInfo?.image} alt={userInfo?.email} />
+      <button
+        className="mt-4 cursor-pointer rounded-2xl bg-black p-2 text-white"
+        onClick={() => handleLogout()}
+      >
+        Logout
+      </button>
+    </div>
   );
 };
 
